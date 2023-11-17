8237c20bebcc377cf82755368aca585121b1cef1
miguelnery <nery.mik@gmail.com>
2023-11-16 21:15:26 -0300

    bird manager

--- BirdManager.c
+++ BirdManager.c
@@ -4,17 +4,60 @@
 #include "raylib.h"
 #include "raymath.h"
 
+#define SIZE 115
+#define SOURCE_RECT (Rectangle){0, 0, 525, 493}
+
 Vector2 position = {0, 0};
 Vector2 velocity = {0, 0};
+Texture2D texture;
+Rectangle birdRectangle = (Rectangle){0, 0, SIZE, SIZE};
 
 void initBird(float x, float y) {
     position = (Vector2){x, y};
     addToGravitySimulation(&velocity);
+    texture = LoadTexture("resources/bird.png");
+}
+
+void initBirdV(Vector2 initPosition) {
+    initBird(initPosition.x, initPosition.y);
+}
+
+Rectangle getBirdRect() {
+    return birdRectangle;
+}
+
+Rectangle centeredOriginRec(Rectangle);
+
+void drawBird() {
+    birdRectangle.x = position.x;
+    birdRectangle.y = position.y;
+    DrawRectanglePro(
+        centeredOriginRec(birdRectangle),
+        Vector2Zero(),
+        0,
+        MAROON
+    );
+    DrawTexturePro(
+        texture,
+        SOURCE_RECT,
+        birdRectangle,
+        (Vector2){birdRectangle.width/2, birdRectangle.height/2},
+        -15,
+        WHITE
+    );
 }
 
 void manageBird() {
     detectJumpInput(&velocity);
     position = Vector2Add(position, velocity);
-    DrawCircleV(position, 15, MAGENTA);
+    drawBird();
 }
 
+Rectangle centeredOriginRec(Rectangle rec) {
+    return (Rectangle) { 
+        rec.x - (rec.width / 2), 
+        rec.y - (rec.height / 2),
+        rec.width,
+        rec.height 
+    };
+}
--- BirdManager.h
+++ BirdManager.h
@@ -1,7 +1,10 @@
 #ifndef BIRD_MANAGER_H
 #define BIRD_MANAGER_H
+#include "raylib.h"
 
+Rectangle getBirdRect();
 void initBird(float, float);
+void initBirdV(Vector2);
 void manageBird();
 
 #endif
