3c7cc6b4b6781ed6c7c1394f858465b90ada91f2
miguelnery <nery.mik@gmail.com>
2023-11-16 21:16:15 -0300

    add convenience properties to Window.h

--- Window.h
+++ Window.h
@@ -1,8 +1,11 @@
 #ifndef WINDOW_H
 #define WINDOW_H
+#include "raylib.h"
 
 #define SCREEN_WIDTH 800
 #define SCREEN_HEIGHT 450
+#define SCREEN_SIZE (Vector2){SCREEN_WIDTH, SCREEN_HEIGHT}
+#define SCREEN_CENTER (Vector2) {SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2}
 
 void initWindow();
 void manageWindow();
